25c3369636410c2ff0bef7c15416083e88805292
Lelena1 <luppol@inbox.ru>
2022-11-21 15:04:25 +0300

    Сделано ДЗ по ООП (урок 2.1): Домашнее задание 1 - задания 1 и 2; Домашнее задание 2 - задание 1; Домашнее задание 3 - задание 1;
    Сделано Дз по Инкапсуляции

---
+++ Flower.java
@@ -0,0 +1,101 @@
+public class Flower {
+    private String flowerName;
+    private String flowerColor;
+    private String country;
+    private double cost;
+    int lifeSpan;
+
+
+    Flower(String flowerName, String flowerColor, String country, double cost, int lifeSpan) {
+
+        if (flowerName != null && !flowerName.isEmpty() && !flowerName.isBlank()) {
+            this.flowerName = flowerName;
+        } else {
+            this.flowerName = "Хризантема";
+        }
+
+        if (flowerColor != null && !flowerColor.isEmpty() && !flowerColor.isBlank()) {
+            this.flowerColor = flowerColor;
+        } else {
+            this.flowerColor = "белый";
+        }
+
+        if (country != null && !country.isEmpty() && !country.isBlank()) {
+            this.country = country;
+        } else {
+            this.country = "Россия";
+        }
+
+        if (cost > 0) {
+            this.cost = cost;
+        } else {
+            this.cost = 1.00;
+        }
+
+        if (lifeSpan > 0) {
+            this.lifeSpan = lifeSpan;
+        } else {
+            this.lifeSpan = 3;
+        }
+
+    }
+
+    void printAllInfoAboutFlowers() {
+        System.out.println(toString());
+    }
+
+    public String getFlowerName() {
+        return flowerName;
+    }
+
+    public String getFlowerColor() {
+        return flowerColor;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setFlowerName(String flowerName) {
+        if (flowerName != null && !flowerName.isEmpty() && !flowerName.isBlank()) {
+            this.flowerName = flowerName;
+        } else {
+            this.flowerName = "Хризантема";
+        }
+    }
+
+    public void setFlowerColor(String flowerColor) {
+        if (flowerColor != null && !flowerColor.isEmpty() && !flowerColor.isBlank()) {
+            this.flowerColor = flowerColor;
+        } else {
+            this.flowerColor = "белый";
+        }
+    }
+
+    public void setCountry(String country) {
+        if (country != null && !country.isEmpty() && !country.isBlank()) {
+            this.country = country;
+        } else {
+            this.country = "Россия";
+        }
+    }
+
+    public void setCost(double cost) {
+        if (cost > 0) {
+            this.cost = cost;
+        } else {
+            this.cost = 1.00;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return flowerName + ", цвет - " + flowerColor + ", страна происхождения - " + country
+                + ", стоимость 1 штуки (рублей): " + String.format("%.2f", cost) + ", срок стояния (дней): "
+                + lifeSpan + ".";
+    }
+}
